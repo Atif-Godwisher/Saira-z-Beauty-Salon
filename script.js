f4c90d16f429436e4008fb63f6b168836b570df3
Atif Hussain <atifh182@gmail.com>
2026-05-23 11:38:25 +0500

    Add SERVICES and VIDEOS objects to script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const SERVICES = {
+Facials: [
+{ name: "Hydra Facial", desc: "Deep cleaning", price: "PKR 4500" }
+],
+Hair: [],
+Makeup: [],
+'Skin Care': [],
+Nails: []
+};
+
+const VIDEOS = {
+requested_instagram: "https://www.instagram.com/reel/DXZaL14jKWV/embed/"
+};
